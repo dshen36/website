8217f38e768f31c533ab7c20afe06e1545d85cd6
dshen36 <dshen36@gatech.edu>
2015-06-11 02:31:03 -0400

    implemented green typing caret
    it blinks. but need it to follow text

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ $(document).ready(function() {
 	    }); 
     });
 });
+
+function caretBlink() {
+    $('#caret').animate({opacity: 0}, 'fast', 'swing').animate({opacity: 1}, 'fast', 'swing');
+}
+
+$(document).ready(function() {
+	setInterval ('caretBlink()', 750);
+});
