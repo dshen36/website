ba1b8dbac8b2365d474086d876910f13823c46c9
dshen36 <dshen200@COTNPL-9S7N3Q1.cable.comcast.com>
2015-06-12 13:43:41 -0600

    landing text pulsates over hover
    -wont stop hovering.

--- script.js
+++ script.js
@@ -10,3 +10,12 @@
 	    }); 
     });
 });*/
+$(document).ready(function(){
+    $('#landing').bind('mouseenter', function() {
+   		this.iid = setInterval(function() {
+       		$("#landing").animate({opacity: 0.2}, 1000, 'linear').animate({opacity: 1}, 1000, 'linear');
+ 		}, 25);
+	}).bind('mouseleave', function(){
+    	this.iid && clearInterval(this.iid);
+	});
+});
